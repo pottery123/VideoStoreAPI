16a652d52eff463289c7e0d549d27fb8b899652f
Sarah <sarahkane682@yahoo.com>
2016-06-15 15:44:33 -0700

    added schema working on seeding data

---
+++ tasks/seed.js
@@ -0,0 +1,18 @@
+var massive = require('massive');
+		var connectionString = 'postgres://localhost/video-store-api';
+		var db = massive.connectSync({connectionString : connectionString});
+		var data = require('../db/seeds/movies.json');
+    //var data = require('../db/seeds/customers.json');
+
+    for (var record of data) {
+   db.movies.save(record, function(err, res) {
+     console.log('saved: ', JSON.stringify(res))
+   })
+}
+  // for (var record of data){ console.log,record.rental_id, record.name, record.reg_date, record.address,
+  //   record.city,record.state,record.postal_code,record.phone,record.acct_credit
+  // db.customers.saveSync({record})
+  //}
+
+
+process.exit()
