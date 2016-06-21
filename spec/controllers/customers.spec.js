638350a657932a626689194f1cedbd3e2b222765
Sarah <sarahkane682@yahoo.com>
2016-06-21 12:58:15 -0700

    first three passing test

---
+++ spec/controllers/customers.spec.js
@@ -0,0 +1,37 @@
+var request = require('request')
+var base_url = "http://localhost:3000/customers"
+
+
+describe("Endpoint at /customers", function () {
+  // it('responds with a 200 status code', function (done) {
+  //   request.get(base_url, function(error, response, body) {
+  //     expect(response.statusCode).toEqual(200)
+  //     done()
+  //   })
+  // })
+
+  describe("the returned json data", function() {
+    it('has the right keys', function(done) {
+      request.get(base_url, function(error, response, body) {
+        var customers = JSON.parse(body)[0]["id"]
+        expect(Object.keys(customers)).toEqual(['id','name','registered_at', 'address', 'city', 'state', 'postal_code', 'phone', 'account_credit'])
+        done()
+      })
+    })
+
+    // it('has the right values for the keys', function(done) {
+    //   request.get(base_url, function(error, response, body) {
+    //     var data = JSON.parse(body)
+    //     expect(data.whatevs).toEqual('whatevs!!!')
+    //     done()
+    //   })
+    // })
+  })
+
+
+
+
+
+
+
+})
